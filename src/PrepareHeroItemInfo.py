75b18dea996a9d6605c966873404d59403ce69ea
Vincent Tsang <yukun.zeng.hit@outlook.com>
2018-04-13 01:07:23 -0500

    ignored recipe, upgrade items new functions for transforming wei's list from org id to new id commented out avg item count baseline improved to 0.73

--- src/PrepareHeroItemInfo.py
+++ src/PrepareHeroItemInfo.py
@@ -1,6 +1,15 @@
-import re
 import json
+import re
 
+def is_recipe(iname):
+    return re.match('recipe_', iname)!=None
+def is_upgrade(iname):
+    return re.match('^[A-Za-z0-9_-]*_[0-9]$', iname)!=None
+def is_consume(iname):
+    return False
+    #return (iname2iid[iname] in consume_iids)
+def is_not_consider(name):
+    return (is_recipe(name) or is_upgrade(name) or is_consume(name))
 # construct item/hero-id based on json data
 # reformat the id sequence if iflag is True
 # iflag=True=>1. id reformat, 2. upgrade item check, 3. repeat name ignore
@@ -15,9 +24,11 @@ def build_name_id_mapping(data, iflag=False):
         iid = data[i]['id']
         # not repeated item/hero
         if name not in id_data:
+            # for item
             if iflag:
                 cost_data[name] = int(data[i]['cost'])
                 id_data[name] = i-repeat
+            # for hero
             else:
                 id_data[name] = int(data[i]['id'])
         else:
@@ -25,14 +36,57 @@ def build_name_id_mapping(data, iflag=False):
             repeat += 1
     return id_data, cost_data
 
-def is_upgrade_item(iid):
-    up_lst = [201, 202, 203, 204, 193, 194, 220]
-    up_dic = {201: 104, 202: 104, 203: 104,
-              204: 104, 193: 106, 194: 106, 220: 48}
-    if iid in up_lst:
-        return up_dic[iid]
-    else:
-        return -1
+def build_iname_iid_map(data):
+    id_data = dict()
+    cost_data = dict()
+    iid_org2new = dict()
+    ignore = 0
+    for i in range(0, len(data)):
+        # substitute prefix part
+        name = re.sub('(npc_dota_hero_|item_)', '', data[i]['name'])
+        iid = data[i]['id']
+        # not repeated item/hero
+        if name not in id_data:
+            # TODO: 
+            # 1. remove recipe item
+            # 2. remove consume item
+            # 3. remove upgraded versions of an item
+
+            if is_not_consider(name):
+                ignore += 1
+            else:
+                cost_data[name] = int(data[i]['cost'])
+                newid = i - ignore
+                id_data[name] = newid
+                iid_org2new[iid] = newid
+        else:
+            print "ignored item: " + str(name) + "\t" + str(iid)
+            ignore += 1
+    return id_data, cost_data, iid_org2new
+
+def build_hname_hid_map(data):
+    id_data = dict()
+    repeat = 0
+    for i in range(0, len(data)):
+        # substitute prefix part
+        name = re.sub('(npc_dota_hero_|item_)', '', data[i]['name'])
+        iid = data[i]['id']
+        # not repeated item/hero
+        if name not in id_data:
+            id_data[name] = int(data[i]['id'])
+        else:
+            print "repeated item: " + str(name) + "\t" + str(iid)
+            repeat += 1
+    return id_data
+
+#def is_upgrade_item(iid):
+#    up_lst = [201, 202, 203, 204, 193, 194, 220]
+#    up_dic = {201: 104, 202: 104, 203: 104,
+#              204: 104, 193: 106, 194: 106, 220: 48}
+#    if iid in up_lst:
+#        return up_dic[iid]
+#    else:
+#        return -1
 
 def reverse_dict(dict):
     return { v: k for k, v in dict.items() }
@@ -41,10 +95,12 @@ def prepare_hero_item_info():
     hero_list = json.load(open('../heroes.json'))['rows']
     item_list = json.load(open('../items.json'))['rows']
 
-    hero_name2id, _ = build_name_id_mapping(hero_list, False)
-    item_name2id, icosts = build_name_id_mapping(item_list, True)
+    #hero_name2id, _ = build_name_id_mapping(hero_list, False)
+    #item_name2id, icosts = build_name_id_mapping(item_list, True)
+    hero_name2id = build_hname_hid_map(hero_list)
+    item_name2id, icosts, iid_org2new = build_iname_iid_map(item_list)
 
     hid2name = reverse_dict(hero_name2id)
     iid2name = reverse_dict(item_name2id)
 
-    return hero_name2id, item_name2id, hid2name, iid2name, icosts
+    return hero_name2id, item_name2id, hid2name, iid2name, icosts, iid_org2new
--- src/main.py
+++ src/main.py
@@ -3,11 +3,32 @@ import os
 import json
 from scipy import spatial as sp
 from PrepareHeroItemInfo import prepare_hero_item_info
+import re
+import PrepareHeroItemInfo as pi
 
 DATA_FILE_PATH = '../data/'
 
 WIN_SCORE = 1.2
 LOSE_SCORE = 0.8
+
+# 267->196 spirit vessel
+# 220->48 travel boots
+# 193->106 necronominon
+syn_iids = [121,123,98,250,252,263,104,141,145,147,110,249,154,158,164,235,185,229,196,242,190,231,206,208,210,212,79,81,90]
+syn_iid_child = {121:[129],123:[69],98:[67,67],250:[98,149],252:[67],263:[102,236,75],104:[77],141:[149],145:[69],147:[170],110:[69,69],249:[152],154:[162,170],158:[166],164:[94],235:[129],185:[73],229:[187],196:[92],242:[86,125],190:[77,77],231:[79,180],206:[73,73],208:[125,143],210:[162],212:[75,88],79:[86,94],81:[88,94],90:[131,94]}
+consume_iids = [216,40,42,43,218,44,241,257,265,237,38,39]
+basic_iids = [21, 31, 37, 45, 55, 57, 59, 61, 56, 58, 60, 13, 15, 17, 19, 23, 25, 27, 29, 215, 12, 240, 52, 51, 53, 54, 3, 5, 7, 9, 11, 2, 4, 6, 8, 10, 244, 182, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 41, 84, 181]
+inter_iids = [48, 92,98, 102, 143, 149, 236, 152, 162, 166, 170, 187, 129,131, 73,75,77, 79,86, 88, 125,67, 69, 94, 180]
+final_iids = [160, 196, 48, 50, 106, 119, 121, 123, 96, 250, 252, 100, 232, 263, 104, 108, 141, 145, 147, 223, 225, 256, 259, 110, 112, 114, 116, 139, 151, 249, 154, 156, 158, 164, 172, 174, 176, 178, 235, 185, 229, 196, 242, 127, 133, 135, 137, 168, 190, 231, 206, 239, 208, 210, 212, 214, 63, 81, 90, 1, 247, 65, 36, 254, 71]
+
+
+# no consumes, no recipe
+def is_consider(iname, mname):
+
+    if mname == 'cost':
+        return item_costs[iname] >= 500
+    else:
+        raise Exception("no consider function is defined!")
 
 # if we do not consider time, rather we consider
 # all the necessary items in all time, then our method should also be effective
@@ -89,7 +110,7 @@ def team_purchase_sim_calc(hp, hp_rec, norm=False, aggr_opt='avg'):
 # assumes: dummy_is_vital(iid)
 # necissity evaluation
 # calculating probability 
-def nec_eva(fpath, model, items, tot_count, icosts, pred=base_rec_h):
+def nec_eva(fpath, model, items, icosts, pred=base_rec_h):
     mcount=0
     sim_sum=0
     for fname in os.listdir(fpath):
@@ -110,9 +131,11 @@ def nec_eva(fpath, model, items, tot_count, icosts, pred=base_rec_h):
             hid=p['hero_id']
             #print "purchase length of hero " + str(hid) + ": " + str(len(purchase))
             for k in purchase:
-                iid=items[k]
-                if icosts[k]>=500:
+                #iid=items[k]
+                if pi.is_recipe(k) or pi.is_upgrade(k) or pi.is_consume(k):
+                    pass
                 #if dummy_is_vital(iid):
+                else:
                     vitem[k]=purchase[k]
             hero_vitem.append(vitem)
             print hid2name[hid]
@@ -172,45 +195,64 @@ def count_purchased_items(purchases, price_threshold=500):
             total_purchase_num += num_purchased
     return total_purchase_num
 
+def calc_base_freq(hname2hid, iname2iid, consider_func='cost'):
+    hero_count = max(hname2hid.values()) + 1
+    item_count = len(iname2iid)
 
-hero_name_id_mapping, item_name_id_mapping, hid2name, iid2name, item_costs = prepare_hero_item_info()
-hero_count = max(hero_name_id_mapping.values()) + 1
-item_count = len(item_name_id_mapping)
+    basic_freq = np.zeros((hero_count, item_count))
 
-basic_freq = np.zeros((hero_count, item_count))
+    # contains the number of items each hero purchased in each game
+    #hero_num_item_records = []
+    #for i in range(hero_count):
+    #    hero_num_item_records.append([])
 
-# contains the number of items each hero purchased in each game
-hero_num_item_records = []
-for i in range(hero_count):
-    hero_num_item_records.append([])
+    for match_file_name in os.listdir(DATA_FILE_PATH):
+        match_file = open(DATA_FILE_PATH + match_file_name)
+        match_data = json.load(match_file)
 
-for match_file_name in os.listdir(DATA_FILE_PATH):
-    match_file = open(DATA_FILE_PATH + match_file_name)
-    match_data = json.load(match_file)
+        for player in match_data['players']:
+            hero_id = player['hero_id']
+            purchases = player['purchase']
 
-    for player in match_data['players']:
-        hero_id = player['hero_id']
-        purchases = player['purchase']
+            #num_purchased_items = count_purchased_items(purchases)
+            #hero_num_item_records[hero_id].append(num_purchased_items)
 
-        num_purchased_items = count_purchased_items(purchases)
-        hero_num_item_records[hero_id].append(num_purchased_items)
+            win = player['isRadiant'] == player['radiant_win']
 
-        win = player['isRadiant'] == player['radiant_win']
+            for item_name in purchases:
+                if item_name in iname2iid:
+                    item_id = iname2iid[item_name]
 
-        for item_name in purchases:
-            item_id = item_name_id_mapping[item_name]
+                    #if is_consider(item_name, consider_func) and purchases[item_name] is not None:
+                    hero_freq = basic_freq[hero_id]
+                    hero_freq[item_id] += WIN_SCORE if win else LOSE_SCORE
 
-            if item_costs[item_name] >= 500 and purchases[item_name] is not None:
-                hero_freq = basic_freq[hero_id]
-                hero_freq[item_id] += WIN_SCORE if win else LOSE_SCORE
+        match_file.close()
 
-    match_file.close()
+#    hero_item_count = []
 
-hero_item_count = []
-for item_count_list in hero_num_item_records:
-    if len(item_count_list) == 0:
-        hero_item_count.append(0)
-    else:
-        hero_item_count.append(int(round(sum(item_count_list) / float(len(item_count_list)))))
+#    for item_count_list in hero_num_item_records:
+#        if len(item_count_list) == 0:
+#            hero_item_count.append(0)
+#        else:
+#            hero_item_count.append(int(round(sum(item_count_list) / float(len(item_count_list)))))
+    return basic_freq
 
-nec_eva('../test/', basic_freq, item_name_id_mapping, hero_item_count, item_costs)
+def lst_dict_iid_org2new(syn_iids, syn_iid_child, consume_iids, basic_iids, inter_iids, final_iids, iid_org2new):
+    syn_iids = [iid_org2new[i] for i in syn_iids]
+    consume_iids = [iid_org2new[i] for i in consume_iids]
+    basic_iids = [iid_org2new[i] for i in basic_iids]
+    inter_iids = [iid_org2new[i] for i in inter_iids]
+    final_iids = [iid_org2new[i] for i in final_iids]
+    new_dict = {}
+    for k, v in syn_iid_child.iteritems():
+        new_dict[iid_org2new[k]]=[iid_org2new[i] for i in v]
+    syn_iid_child = new_dict
+    return syn_iids, syn_iid_child, consume_iids, basic_iids, inter_iids, final_iids
+
+hname2hid, iname2iid, hid2name, iid2name, item_costs, iid_org2new = prepare_hero_item_info()
+syn_iids, syn_iid_child, consume_iids, basic_iids, inter_iids, final_iids = lst_dict_iid_org2new(syn_iids, syn_iid_child, consume_iids, basic_iids, inter_iids, final_iids, iid_org2new)
+basic_freq = calc_base_freq(hname2hid, iname2iid)
+
+nec_eva('../test/', basic_freq, iname2iid, item_costs)
+
